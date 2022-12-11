9cd16cf671dfa6ba3e9f7876a4faaebc627d7716
Niles O'Neil <nilesoneil@gmail.com>
2022-12-11 07:15:52 -0800

    first test written

---
+++ VendorOrder.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,22 @@
+sing Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorOrder.Models;
+using System;
+
+namespace VendorOrder.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("Test", "Testing Stuff", "17", "November");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }

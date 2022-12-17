a063bc88eacc82cb2f2b1e9e7ad64e251c4b5f65
Niles O'Neil <nilesoneil@gmail.com>
2022-12-17 13:53:40 -0800

    updated views folder

--- VendorOrder/Program.cs
+++ VendorOrder/Program.cs
@@ -1,19 +1,28 @@
-using System.IO;
-using Microsoft.AspNetCore.Hosting;
+using VendorOrder.Models;
 
-namespace VendorOrder
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<VendorContext>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
-      var host = new WebHostBuilder()
-        .UseKestrel()
-        .UseContentRoot(Directory.GetCurrentDirectory())
-        .UseIISIntegration()
-        .Build();
-
-      host.Run();
-    }
-  }
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ VendorOrder/Views/Vendor/Create.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BakeryVendor.Views.Vendor;
+
+public class Create : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}

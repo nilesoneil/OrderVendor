f0c1762693a81717ef0cdc9b5766ec133f0a0b09
Niles O'Neil <nilesoneil@gmail.com>
2022-12-17 15:23:05 -0800

    all tests are working

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -7,7 +7,18 @@ namespace VendorOrder.Tests.ModelTests;
 public class VendorContextTest
 {
     [TestMethod]
-    public void VendorContext_AddsVendor()
+    public void VendorContext_CreatesInstanceOfVendor_AddsVendor()
+    {
+        VendorContext vc = new VendorContext();
+        Vendor v = new Vendor();
+        vc.AddToVendors(v);
+
+        List<Vendor> otherV = vc.GetAllVendors();
+        Assert.AreEqual(1, otherV.Count);
+    }
+
+    [TestMethod]
+    public void VendorContext_FindsVendorById_GetVendorById()
     {
         VendorContext vc = new VendorContext();
         Vendor v = new Vendor();
@@ -15,5 +26,39 @@ public class VendorContextTest
 
         Vendor otherV = vc.GetVendorById(v.id);
         Assert.AreSame(v, otherV);
+    }
+    [TestMethod]
+    public void VendorModel_CanAddOrder()
+    {
+        Vendor v = new Vendor
+        {
+            description = "GOOD BAKERY"
+        };
+
+        Order o = new Order()
+        {
+            Title = "Some Order"
+        };
+        
+        v.orders.Add(o);
+        
+        Assert.AreEqual(1, v.orders.Count);
+    }
+    [TestMethod]
+    public void VendorModel_CanAddOrderAndReturnSameOrder()
+    {
+        Vendor v = new Vendor
+        {
+            description = "GOOD BAKERY"
+        };
+
+        Order o = new Order()
+        {
+            Title = "Some Order"
+        };
+        
+        v.orders.Add(o);
+        
+        Assert.AreSame(v.orders[0], o);
     }
 }

40866bf5afb928c6318ec26183773301e7a2a00e
Niles O'Neil <nilesoneil@gmail.com>
2022-12-17 12:19:48 -0800

    updated Order.cs

--- VendorOrder/Models/Order.cs
+++ VendorOrder/Models/Order.cs
@@ -4,29 +4,17 @@ namespace VendorOrder.Models
 {
   public class Order
   {
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string Price { get; set; }
-    public string Quantity { get; set; }
-    public string Date { get; set; }
-    public int Id { get; }
-    private static List<Order> _instances = new List<Order> { };
+    public string Title { get; }
+    public string Description { get;  }
+    public float Price { get; }
+    public DateTime Date { get; }
 
-    public Order(string title, string description, string quantity, string price, string date)
+    public Order(string title, string description, float price, DateTime date)
     {
       Title = title;
       Description = description;
-      Quantity = quantity;
       Price = price;
       Date = date;
-      _instances.Add(this);
-      Id = _instances.Count;
-    }
-
-
-    public static void ClearAll()
-    {
-      _instances.Clear();
     }
     
   }
